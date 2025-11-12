54b676ad4456aed7455c50a0b97a02e7bcf89e37
jddunn <johnnyfived@protonmail.com>
2025-11-12 18:12:51 +0000

    Deploying to gh-pages from @ manicinc/manicinc@42f8e448988a4f8029765496b80bc950c08a9cb5 🚀

---
+++ _next/static/manic-agency-1762971107263/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e1bb9bc3dcbdd845.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
